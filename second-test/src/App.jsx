9268b26e6836d830011b081ddeea3ae6ca667eff
Ye Thiha <mightycryptowall@gmail.com>
2022-12-23 19:59:57 +0630

    init

---
+++ second-test/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React Codeing Test</h1>
+      <form className="card">
+        
+       
+      </form>
+    </div>
+  )
+}
+
+export default App
